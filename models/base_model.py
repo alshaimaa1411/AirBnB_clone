481606334e1270703ecaeea69e38d547b0ab194a
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-05-15 22:30:31 +0300

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -22,21 +22,10 @@ class BaseModel:
     
     def save(self):
         self.updated_at = datetime.now()
+        self.updated_at = self.updated_at.isoformat()
     
     def to_dict(self):
         b_dict = self.__dict__.copy()
         b_dict[__class__]= type(self).__name__
         return b_dict
     
-
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
