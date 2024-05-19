0122fe182a95722662058d76b63311707701aee2
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-05-19 16:05:15 +0300

    a

--- models/base_model.py
+++ models/base_model.py
@@ -12,9 +12,7 @@ class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.created_at = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
         self.updated_at = datetime.now()
-        self.updated_at = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
     
     def __str__(self):
         """ print id , name"""
@@ -23,11 +21,13 @@ class BaseModel:
     def save(self):
         """ save created time"""
         self.updated_at = datetime.now()
-        self.updated_at = self.updated_at.isoformat()
 
     def to_dict(self):
         """ return dicit"""
         b_dict = self.__dict__.copy()
         b_dict["__class__"]= type(self).__name__
+        b_dict["created_at"] = b_dict["created_at"].isoformat()
+        b_dict["updated_at"] = b_dict["updated_at"].isoformat()
         return b_dict
-    
+
+
