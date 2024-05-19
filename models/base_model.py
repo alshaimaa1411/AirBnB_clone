4fff376c38c909a6b0a4905e22e6c1608a45558c
Boodenator <boode1000@gmail.com>
2024-05-19 14:46:05 -0700

    bug fix

--- models/base_model.py
+++ models/base_model.py
@@ -6,32 +6,28 @@ the base class for all models."""
 from models import storage
 from datetime import datetime
 import uuid
- 
+
 
 class BaseModel:
-    """The Base class of all Classes"""
+ """The Base class of all Classes"""
 
-    def __init__(self, *args, **kwargs):
+ def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ('created_at', 'updated_at'):
                         setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                    else:
-                        self.__dict__[key] = value
+            else:
+              self.__dict__[key] = value
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now() 
+            self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
         storage.save()
 
-        
-
-
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-
 
     def save(self):
         self.updated_at = datetime.now()
