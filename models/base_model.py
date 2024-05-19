a180aec69fbfa7a6c82e56acc296018f64cdfc11
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-05-19 16:47:14 +0300

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -13,8 +13,12 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         if kwargs:
-            for item in kwargs:
-                self.__dict__[item] = kwargs[item]
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        self.__dict__[key] = value
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now() 
