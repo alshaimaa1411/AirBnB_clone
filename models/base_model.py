b6e8bf1bc4c7e4c9c58dce54a2c21f07a132ac33
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-05-19 15:09:39 +0300

    a

--- models/base_model.py
+++ models/base_model.py
@@ -3,35 +3,30 @@
 This will define the BaseModel class functioning as
 the base class for all models."""
 
-import models
+
+import uuid
 from datetime import datetime
-from uuid import uuid4
- 
 
 class BaseModel:
     """The Base class of all Classes"""
-
-    def __init__(self, *args, **kwargs):
-        if kwargs:
-            for item in kwargs:
-                self.__dict__[item] = kwargs[item]
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.created_at = self.created_at.isoformat()
-            self.updated_at = datetime.now()
-            self.updated_at = self.updated_at.isoformat()
-            storage.new(self)
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = datetime.now()
+        self.updated_at = self.updated_at.isoformat()
     
     def __str__(self):
+        """ print id , name"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
     
     def save(self):
+        """ save created time"""
         self.updated_at = datetime.now()
         self.updated_at = self.updated_at.isoformat()
-        storage.save()
-    
+
     def to_dict(self):
+        """ return dicit"""
         b_dict = self.__dict__.copy()
         b_dict["__class__"]= type(self).__name__
         return b_dict
